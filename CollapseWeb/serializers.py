2d4d41779b022fbbf939e0db976adcaf475a5ec5
dest4590 <80628386+dest4590@users.noreply.github.com>
2024-04-30 17:12:39 +0300

    some changes

--- CollapseWeb/serializers.py
+++ CollapseWeb/serializers.py
@@ -2,9 +2,22 @@ from rest_framework import serializers, viewsets, routers
 from .models import Client
 
 class ClientSerializer(serializers.HyperlinkedModelSerializer):
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = Client
-        fields = ['id', 'name', 'version', 'link']
+        fields = ['id', 'name', 'version', 'link', 'created_at', 'updated_at']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            self.fields['created_at'].read_only = False
+            self.fields['updated_at'].read_only = False
+        else:
+            self.fields.pop('created_at', None)
+            self.fields.pop('updated_at', None)
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
