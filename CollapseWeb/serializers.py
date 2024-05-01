7f04c63383bde6e1b300de2181a8908a9c8983f9
dest4590 <80628386+dest4590@users.noreply.github.com>
2024-05-01 22:45:51 +0300

    added /info api endpoint

--- CollapseWeb/serializers.py
+++ CollapseWeb/serializers.py
@@ -1,4 +1,6 @@
-from rest_framework import serializers, viewsets, routers
+from collections import Counter
+from rest_framework import routers, serializers, viewsets, status
+from rest_framework.response import Response
 from .models import Client
 
 class ClientSerializer(serializers.HyperlinkedModelSerializer):
@@ -6,9 +8,27 @@ class ClientSerializer(serializers.HyperlinkedModelSerializer):
         model = Client
         fields = ['id', 'name', 'version', 'link', 'created_at', 'updated_at']
 
-class ClientViewSet(viewsets.ModelViewSet):
+class BaseClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
 
+class ClientViewSet(BaseClientViewSet):
+    pass
+
+class InfoViewSet(viewsets.ViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+    def list(self, request):
+        versions_counter = Counter(self.queryset.values_list('version', flat=True))
+        
+        data = {
+            'count': self.queryset.count(),
+            'versions': dict(versions_counter),
+        }
+
+        return Response(data)
+
 router = routers.DefaultRouter()
 router.register(r'clients', ClientViewSet)
+router.register(r'info', InfoViewSet, basename='info')
--- Core/urls.py
+++ Core/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import include, path, re_path
 from django.views.static import serve
+
+from CollapseWeb import serializers, views
+
 from .settings import MEDIA_ROOT, STATIC_ROOT
-from CollapseWeb import views, serializers
 
 urlpatterns = [
     path('', views.index),
+    path('admin/', admin.site.urls),
+    
     path('api/', views.api),
     path('api/', include(serializers.router.urls)),
-    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     re_path(r'^upload/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
